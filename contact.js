7711b591bf8d43c101147df47f5603c27102526a
Shaun Newman <shaun.newman.p@gmail.com>
2024-11-20 09:41:17 -0500

    completed auth form on codecademy

---
+++ contact.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function Contact() {
+  const password = 'swordfish';
+  const [authorized, setAuthorized] = useState(false);
+
+  function handleSubmit(e) {
+    const enteredPassword = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = enteredPassword == password;
+    setAuthorized(auth)
+  }
+
+  let login = (
+    <form action="#" onSubmit={handleSubmit}>
+    <input type="password" placeholder="password">
+    </input>
+    <input type="submit">
+    </input>
+    </form>
+    );
+
+    let contactInfo = (
+      <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
+    );
+
+  return (
+      <div id="authorization">
+        <h1>
+        {
+        authorized ? "Contact" : "Enter Password"
+        }
+        </h1>
+        {
+        authorized ? contactInfo : login
+        }
+      </div>
+  );
+}
+
+export default Contact;
